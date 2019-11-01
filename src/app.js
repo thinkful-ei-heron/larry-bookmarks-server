0de602ef8b9155be512efc4450c97f50ceb6043a
larry <larryzie@gmail.com>
2019-11-01 14:10:14 -0500

    Added logger and authentication

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const winston = require('winston');
+
+const { NODE_ENV } = require('./config');
+const logger = require('./logger');
 
 const app = express()
 
@@ -14,6 +17,17 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);    
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+});
 
 app.post('/', (req, res) => {
   res
